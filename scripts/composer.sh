f33f0714b2caa6bc6c0d45e623a860b6d2ff985f
xuantain <xuantain@gmail.com>
2019-05-25 19:51:17 +0700

    Integrate GraphQL

---
+++ scripts/composer.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker run --rm \
+    --user $(id -u):$(id -g) \
+    --volume $PWD:/var/www \
+    --volume $HOME/.composer:/.composer \
+    --workdir /var/www \
+    --entrypoint "composer" \
+    digitalocean.com/php "$@"
---
+++ scripts/dev.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+ [ -f ".env" ] || $(echo Please make an .env file and run: php artisan key:generate --env=dev; exit 1)
+export $(cat .env | grep -v ^# | xargs);
+echo Starting services
+docker-compose up -d
+echo Host: 127.0.0.1
+until docker-compose exec mariadb mysql -h 127.0.0.1 -u $DB_USERNAME -p$DB_PASSWORD -D $DB_DATABASE --silent -e "show databases;"
+do
+  echo "Waiting for database connection..."
+  sleep 5
+done
+echo Installing dependencies
+./scripts/npm.sh install
+./scripts/composer.sh install
+echo Seeding database
+rm -f bootstrap/cache/*.php
+docker-compose exec app php artisan migrate --env=dev && echo Database migrated
+docker-compose exec app php artisan db:seed --env=dev && echo Database seeded
