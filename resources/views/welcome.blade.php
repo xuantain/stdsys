f112c294e771da2d727543781cb46088d806543e
xuantain <xuantain@gmail.com>
2019-07-01 23:50:06 +0700

    Using Lighthouse instead of graphiql

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <a href="{{url()->current() . '/graphiql'}}">
+                    <a href="{{url()->current() . '/graphql-playground'}}">
                         {{$appName}}
                     </a>
                 </div>
